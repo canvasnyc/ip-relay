b98fa9d4c135333fa8d7614301a2fd7e131bacdb
toddmazierski <toddmazierski@gmail.com>
2011-06-04 19:05:56 -0400

    Fixed bug: the .json files do not need to be parsed

--- tests/TestBeanstalkIntegration.rb
+++ tests/TestBeanstalkIntegration.rb
@@ -16,55 +16,55 @@ class TestBeanstalkIntegration < Test::Unit::TestCase
   end
 
   def test_commit_no_bugs
-    commit = JSON.parse(File.read('examples/beanstalk/commit_no_bugs.json'))
+    commit = File.read('examples/beanstalk/commit_no_bugs.json')
     post '/beanstalk/commit', params = {:commit => commit}
     assert last_response.ok?
   end
 
   def test_commit_1_bug
-    commit = JSON.parse(File.read('examples/beanstalk/commit_1_bug.json'))
+    commit = File.read('examples/beanstalk/commit_1_bug.json')
     post '/beanstalk/commit', params = {:commit => commit}
     assert last_response.ok?
   end
 
   def test_commit_1_bug_1_status
-    commit = JSON.parse(File.read('examples/beanstalk/commit_1_bug_1_status.json'))
+    commit = File.read('examples/beanstalk/commit_1_bug_1_status.json')
     post '/beanstalk/commit', params = {:commit => commit}
     assert last_response.ok?
   end
 
   def test_commit_2_bugs_1_status
-    commit = JSON.parse(File.read('examples/beanstalk/commit_2_bugs_1_status.json'))
+    commit = File.read('examples/beanstalk/commit_2_bugs_1_status.json')
     post '/beanstalk/commit', params = {:commit => commit}
     assert last_response.ok?
   end
 
   def test_payload_no_bugs
-    payload = JSON.parse(File.read('examples/beanstalk/payload_no_bugs.json'))
+    payload = File.read('examples/beanstalk/payload_no_bugs.json')
     post '/beanstalk/payload', params = {:payload => payload}
     assert last_response.ok?
   end
 
   def test_payload_1_bug
-    payload = JSON.parse(File.read('examples/beanstalk/payload_1_bug.json'))
+    payload = File.read('examples/beanstalk/payload_1_bug.json')
     post '/beanstalk/payload', params = {:payload => payload}
     assert last_response.ok?
   end
 
   def test_payload_1_bug_1_status
-    payload = JSON.parse(File.read('examples/beanstalk/payload_1_bug_1_status.json'))
+    payload = File.read('examples/beanstalk/payload_1_bug_1_status.json')
     post '/beanstalk/payload', params = {:payload => payload}
     assert last_response.ok?
   end
 
   def test_payload_2_bugs_1_status
-    payload = JSON.parse(File.read('examples/beanstalk/payload_2_bugs_1_status.json'))
+    payload = File.read('examples/beanstalk/payload_2_bugs_1_status.json')
     post '/beanstalk/payload', params = {:payload => payload}
     assert last_response.ok?
   end
 
   def test_payload_too_large
-    payload = JSON.parse(File.read('examples/beanstalk/payload_too_large.json'))
+    payload = File.read('examples/beanstalk/payload_too_large.json')
     post '/beanstalk/commit', params = {:payload => payload}
     assert last_response.ok?
   end

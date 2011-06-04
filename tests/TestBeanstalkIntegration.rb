a59abd5690d101e53bc4cf3ea08a5acd9711495c
toddmazierski <toddmazierski@gmail.com>
2011-06-04 16:41:02 -0400

    For "commits" (SVN) and "payloads" (git), created Beanstalk integration tests and example JSON files (copied from Beanstalk's "Web hooks Integration" page)

---
+++ tests/TestBeanstalkIntegration.rb
@@ -0,0 +1,72 @@
+Dir.chdir '../'
+require 'rubygems'
+require 'sinatra'
+require 'ip-relay'
+require 'json'
+require 'test/unit'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+class TestBeanstalkIntegration < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_commit_no_bugs
+    commit = JSON.parse(File.read('examples/beanstalk/commit_no_bugs.json'))
+    post '/beanstalk/commit', params = {:commit => commit}
+    assert last_response.ok?
+  end
+
+  def test_commit_1_bug
+    commit = JSON.parse(File.read('examples/beanstalk/commit_1_bug.json'))
+    post '/beanstalk/commit', params = {:commit => commit}
+    assert last_response.ok?
+  end
+
+  def test_commit_1_bug_1_status
+    commit = JSON.parse(File.read('examples/beanstalk/commit_1_bug_1_status.json'))
+    post '/beanstalk/commit', params = {:commit => commit}
+    assert last_response.ok?
+  end
+
+  def test_commit_2_bugs_1_status
+    commit = JSON.parse(File.read('examples/beanstalk/commit_2_bugs_1_status.json'))
+    post '/beanstalk/commit', params = {:commit => commit}
+    assert last_response.ok?
+  end
+
+  def test_payload_no_bugs
+    payload = JSON.parse(File.read('examples/beanstalk/payload_no_bugs.json'))
+    post '/beanstalk/payload', params = {:payload => payload}
+    assert last_response.ok?
+  end
+
+  def test_payload_1_bug
+    payload = JSON.parse(File.read('examples/beanstalk/payload_1_bug.json'))
+    post '/beanstalk/payload', params = {:payload => payload}
+    assert last_response.ok?
+  end
+
+  def test_payload_1_bug_1_status
+    payload = JSON.parse(File.read('examples/beanstalk/payload_1_bug_1_status.json'))
+    post '/beanstalk/payload', params = {:payload => payload}
+    assert last_response.ok?
+  end
+
+  def test_payload_2_bugs_1_status
+    payload = JSON.parse(File.read('examples/beanstalk/payload_2_bugs_1_status.json'))
+    post '/beanstalk/payload', params = {:payload => payload}
+    assert last_response.ok?
+  end
+
+  def test_payload_too_large
+    payload = JSON.parse(File.read('examples/beanstalk/payload_too_large.json'))
+    post '/beanstalk/commit', params = {:payload => payload}
+    assert last_response.ok?
+  end
+
+end

1e9cc70ec752cb4fc979708a4b3a156e79db89c2
toddmazierski <toddmazierski@gmail.com>
2011-06-04 23:58:56 -0400

    Added Beanstalk routes, Bugzilla destination

---
+++ lib/Beanstalk.rb
@@ -0,0 +1,38 @@
+post '/beanstalk/commit' do
+  commit = JSON.parse params[:commit]
+  commands = interpret commit["message"]
+  commands.each do |command|
+    command[:origin] = "Beanstalk"
+    command[:commit] = commit
+    command[:comment] =
+      "A commit has been made (r#{commit["revision"]}) by " +
+      "#{commit["author_full_name"]} that references this bug.\n\n" +
+      "Comment: #{commit["message"]}\n\n" +
+      "Changeset URL: #{commit["changeset_url"]}"
+  end
+  execute commands
+  respond :commands => commands
+end
+
+post '/beanstalk/payload' do
+  payload = JSON.parse params[:payload]
+  commands = []
+  unless payload["push_is_too_large"]
+    payload["commits"].first.each do |id, commit|
+      commands << interpret(commit["message"]).each do |command|
+        command[:origin] = "Beanstalk"
+        command[:commit] = commit
+        command[:comment] =
+          "A commit has been made (#{commit['id'][0..7]}) by " +
+          "#{commit["author"]["name"]} that references this bug.\n\n" +
+          "Comment: #{commit["message"]}\n\n" +
+          "Changeset URL: #{commit["url"][0..-33]}"
+      end
+    end
+  end
+  execute commands
+  respond :commands => commands
+end
+
+class Beanstalk
+end
---
+++ lib/Bugzilla.rb
@@ -0,0 +1,39 @@
+require 'xmlrpc/client'
+
+create_destination :bug, 'Bugzilla'
+
+class Bugzilla
+
+  @@server = XMLRPC::Client.new2 settings.bugzilla[:url]
+
+  def self.execute(command)
+    if command[:status].nil?
+      add_comment command[:bug], command[:comment]
+    else
+      update_bug command[:bug], command[:comment], command[:status]
+    end
+  end
+
+  def self.auth_args
+    {
+    :Bugzilla_login => settings.bugzilla[:login],
+    :Bugzilla_password => settings.bugzilla[:password]
+    }
+  end
+
+  def self.add_comment(id, comment)
+    args = {:id => id, :comment => comment}
+    @@server.call "Bug.add_comment", args.merge(auth_args)
+  end
+
+  def self.update_bug(id, comment, status)
+    args = {
+      :ids => id,
+      :comment => {:body => comment},
+      :status => status.upcase,
+      :resolution => 'FIXED'
+    }
+    @@server.call "Bug.update", args.merge(auth_args)
+  end
+
+end

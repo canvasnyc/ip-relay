6121a5acf5ce4259b0e6ae1e5b4cc84dd107eb31
toddmazierski <toddmazierski@gmail.com>
2011-06-23 11:53:21 -0400

    Modified email subject to only include the first line of the comment

--- lib/destinations/email.rb
+++ lib/destinations/email.rb
@@ -16,7 +16,7 @@ class Email
     result = Pony.mail(
       :from => settings.smtp[:username],
       :to => args[:to],
-      :subject => args[:comment].gsub("\n", ""),
+      :subject => args[:comment].split("\n").first,
       :body => args[:comment],
       :via => :smtp,
       :via_options => {

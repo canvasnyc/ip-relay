7690d6434bf94a1103c139f06b7b19f1f255637b
Todd Mazierski <toddmazierski@gmail.com>
2011-09-20 20:03:57 -0400

    Added logging so alerts only appear once an hour.

--- lib/origins/downtime.rb
+++ lib/origins/downtime.rb
@@ -3,14 +3,67 @@ post '/downtime' do
   puts "Downtime alert received at #{Time.now}"
   puts request.body.read
   alert = JSON.parse params[:alert]
-  commands = interpret alert["commands"]
-  commands.each do |command|
-    command[:origin] = {
-      :name => "Downtime",
-      :alert => alert
-    }
-    command[:args][:comment] =
-      "#{alert['site']} #{alert['environment']} at #{alert['url']} is down: #{alert['message']}"
+  commands = []
+  downtime = Downtime.new 'downtime'
+  unless downtime.is_logged?(alert)
+    commands = interpret alert["commands"]
+    commands.each do |command|
+      command[:origin] = {
+        :name => "Downtime",
+        :alert => alert
+      }
+      command[:args][:comment] =
+        "#{alert['site']} #{alert['environment']} at #{alert['url']} is down: #{alert['message']}"
+    end
   end
   respond execute commands
+end
+
+class Downtime
+
+  def lookback
+    '-1 HOUR'
+  end
+
+  def initialize(name)
+    @db_file = File.join(settings.db_path, name + '.db')
+  end
+
+  def is_logged?(alert)
+    db_exists? ? db_open : db_create
+    result = find alert
+    if result.empty?
+      log alert
+      false
+    else
+      puts "*" * 79
+      puts "Ignoring alert: #{alert["id"]} #{alert["environment"]}"
+      puts "Was already processed on: #{result.first[2]}"
+      puts "*" * 79
+      true
+    end
+  end
+
+ private
+
+  def db_exists?
+    File.exist? @db_file
+  end
+
+  def db_open
+    @db = SQLite3::Database.new @db_file
+  end
+
+  def db_create
+    db_open
+    @db.execute "CREATE TABLE `alerts` (`id` TEXT, `environment` TEXT, `logged_at` TEXT)"
+  end
+
+  def log(alert)
+    @db.execute "INSERT INTO `alerts` VALUES (\"#{alert["id"]}\", \"#{alert["environment"]}\", DATETIME('NOW'))"
+  end
+
+  def find(alert)
+    @db.execute "SELECT * FROM `alerts` WHERE `id`=\"#{alert["id"]}\" AND `environment`=\"#{alert["environment"]}\" AND `logged_at` >= DATETIME('NOW', '#{lookback}') LIMIT 1"
+  end
 end
--- test/test_downtime_integration.rb
+++ test/test_downtime_integration.rb
@@ -4,17 +4,30 @@ require 'test/unit'
 
 ENV['RACK_ENV'] = 'test'
 
-class TestHoptoadIntegration < Test::Unit::TestCase
+class TestDowntimeIntegration < Test::Unit::TestCase
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
   end
 
-  def test_alert
+  def setup
+    puts "Resetting logged alerts..."
+    file = File.join(settings.db_path, 'downtime.db')
+    File.delete(file) if File.exists?(file)
+  end
+
+  def test_1_alert
     alert = File.read(File.expand_path('../examples/downtime/alert.json', File.dirname(__FILE__)))
     post '/downtime', params = {:alert => alert}
     assert last_response.ok?
   end
 
+  def test_2_alerts
+    alert = File.read(File.expand_path('../examples/downtime/alert.json', File.dirname(__FILE__)))
+    2.times {
+      post '/downtime', params = {:alert => alert}
+      assert last_response.ok?
+    }
+  end
 end
